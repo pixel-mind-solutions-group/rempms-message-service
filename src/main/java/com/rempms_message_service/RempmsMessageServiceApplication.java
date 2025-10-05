4762b9a3db865f7fb65b1aaefe448beab0592e33
Maleesha Sandakalum <maleeshaGitHub0000@gmail.com>
2025-10-05 21:00:12 +0530

    initial changes

---
+++ src/main/java/com/rempms_message_service/RempmsMessageServiceApplication.java
@@ -0,0 +1,13 @@
+package com.rempms_message_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RempmsMessageServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RempmsMessageServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rempms_message_service/RempmsMessageServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rempms_message_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RempmsMessageServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
